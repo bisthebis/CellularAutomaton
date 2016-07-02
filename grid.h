fbdb08f780a8232216c769a35f2d6090853ed35b
bisthebis <bism25@hotmail.com>
2016-07-02 14:32:06 +0200

    Documentation for Grid.h

--- grid.h
+++ grid.h
@@ -1,3 +1,18 @@
+/**
+  *     Data class for managing state of a rectangular cellular automaton.
+  *     Use fill(func) method to go to the next generation, using "func" as a function taking a reference to the Grid, x and y coordinates, and changes the grid on that place.
+  *     Every time fill is called, a "hasChanged" signal is emitted, with "this" pointer as parameter
+  *     At the moment, grid size must be know at construction time
+  *
+  *     The class provides method "GameOfLifeStep()", which calls "fill" with Conway's Game of Life's rule as argument.
+  *     It also provides "print" method, showing the grid using std::cout. It's main purpose is debugging
+  *
+  *     @author Boris Martin
+  *     @version 0.2
+  *
+  */
+
+
 #ifndef GRID_H
 #define GRID_H
 
@@ -27,6 +42,7 @@ class Grid : public QObject
         CoordType getHeight() const {return height;}
 
         const char outOfBoundsValue = 0;
+
         template <class GridFiller> void fill(GridFiller func)  //Takes a function convertible to a std::function<void(Grid&, CoordType, CoordType)>
                                                                 //This function must update (via setNextValueAt) any cell knowing it's coord. It will be called with all possibles coords. Then, update is called.
                                                                 //It is allowed to read current state, but it's user responsibility to ensure current is valid
@@ -45,8 +61,8 @@ class Grid : public QObject
         }
 
     private:
-        CoordType width = 0;
-        CoordType height = 0;
+        const CoordType width = 0;
+        const CoordType height = 0;
         std::vector<char> current;
         std::vector<char> next;
 
