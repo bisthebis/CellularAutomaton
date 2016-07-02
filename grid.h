00fcbb83423a8a547a856946aa4427d71593980e
bisthebis <bism25@hotmail.com>
2016-07-02 23:43:22 +0200

    ISO compatible signal + Grid with Doxygen doc

--- grid.h
+++ grid.h
@@ -1,16 +1,11 @@
 /**
-  *     Data class for managing state of a rectangular cellular automaton.
-  *     Use fill(func) method to go to the next generation, using "func" as a function taking a reference to the Grid, x and y coordinates, and changes the grid on that place.
-  *     Every time fill is called, a "hasChanged" signal is emitted, with "this" pointer as parameter
-  *     At the moment, grid size must be know at construction time
+  *     \file grid.h
+  *     \author Boris Martin
+  *     \brief Class handling a cellular automaton as data.
   *
-  *     The class provides method "GameOfLifeStep()", which calls "fill" with Conway's Game of Life's rule as argument.
-  *     It also provides "print" method, showing the grid using std::cout. It's main purpose is debugging
   *
-  *     Can now paint itself on any QPaintDevice;
   *
-  *     @author Boris Martin
-  *     @version 0.3
+  *
   *
   */
 
@@ -23,6 +18,24 @@
 
 class QPaintDevice;
 
+
+/**     @class Grid grid.h grid.cpp
+ *      @brief The Grid class
+ *
+ *
+ *      Data class for managing state of a rectangular cellular automaton.
+ *      Use fill(func) method to go to the next generation, using "func" as a function taking a reference to the Grid, x and y coordinates, and changes the grid on that place.
+ *      Every time fill is called, a "hasChanged" signal is emitted, with "this" pointer as parameter
+ *      At the moment, grid size must be know at construction time
+ *
+ *      The class provides method "GameOfLifeStep()", which calls "fill" with Conway's Game of Life's rule as argument.
+ *      It also provides "print" method, showing the grid using std::cout. It's main purpose is debugging
+ *
+ *      Can now paint itself on any QPaintDevice;
+ *
+ */
+
+
 class Grid : public QObject
 {
 
@@ -32,11 +45,38 @@ class Grid : public QObject
         Q_OBJECT
 
     signals:
-        hasChanged(Grid* self);
+        /**
+         * @brief Signal when "current" data changes
+         * @param self : pointer to the grid, a.k.a "this"
+         */
+        void hasChanged(Grid* self);
 
     public:
+        /**
+         * @brief Constructor
+         * @param width : number of columns in the grid. Can't be changed later
+         * @param height : number of rows in the grid. Can't be changed later
+         * @param outOfBoundsValue : Value returned whenuser tries to get value bordering the grid.
+         * @param parent : parent in QObject hierarchy
+         */
         Grid(CoordType width = 0, CoordType height = 0, char outOfBoundsValue = 0, QObject* parent = nullptr);
+
+        /**
+         * @brief Gets current value at (x, y)
+         * @param x : absciss of the queried value
+         * @param y : ordinate of the queried value
+         * @return either current[x+y*width] or outOfBoundsValue, deepending on whether x and y are in range [0, width[ and [0, height[
+         */
         char getValueAt(CoordType x, CoordType y) const;
+
+        /**
+         * @brief set next value "value" at (x, y). It will be effective when update() gets called.
+         * @param x : absciss of the changed value
+         * @param y : ordinate of the changed value
+         * @param value : new value1
+         *
+         * Does nothing is out of range [0, width[ or [0, height[
+         */
         void setNextValueAt(CoordType x, CoordType y, char value);
         void update(); //Swap current and next
 
@@ -79,6 +119,12 @@ class Grid : public QObject
         static void GameOfLifeFiller(Grid& grid, int x, int y);
 
     public slots:
+
+        /**
+         * @brief Calls fill(func) with Conway's Game of Life's rule
+         *
+         * This rule is the static public method GameOfLifeFiller
+         */
         void GameOfLifeStep();
 };
 
